9b625f3305f34c1f2adccfa973e3d9d2d3480244
EarmarkSolution <sona.earmark@gmail.com>
2023-06-20 10:12:50 +0530

    Interview Schedule design

--- screens/Employee_pages/Hiring_screens/Interview_status.js
+++ screens/Employee_pages/Hiring_screens/Interview_status.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native'
 import COLORS from '../../../constants/theme'
 import InterviewStatusList from '../../../data/InterviewStatusList'
@@ -6,39 +6,54 @@ import LeaveBalanceList from '../../../data/LeaveBalanceList'
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import candidateIcon from '../../../assets/images/candidateIcon.png'
+import axios from 'axios'
 
 
-const Interview_status = () => {
+const Interview_status = (props) => {
+    const { navigation } = props;
+    const [interViewDetail, setInterViewDetail] = useState()
+    const [status, setStatus] = useState('P')
+
+    const fetchInterviewData = () => {
+        axios.post(`http://192.168.1.169:7038/api/User/InterviewList`, {
+            userId: '10011',
+            operFlag: 'V',
+        })
+            .then(response => {
+                const returnedData = response?.data?.Result;
+                setInterViewDetail(returnedData)
+            });
+    };
+
+    useEffect(() => {
+        fetchInterviewData();
+    }, [])
 
     function CandidateList(props) {
 
-        const id = props.id, name = props.name, designation = props.designation, timing = props.timing, date = props.date
+        const id = props.id, name = props.name, designation = props.designation, interviewStart = props.startTime, interviewEndTime = props.endTime, date = props.date
 
         return (
-            <TouchableOpacity key={id} style={{ width: '100%' }}>
+            <TouchableOpacity key={id} style={{ width: '100%', padding: 4 }} onPress={() => navigation.navigate('Candidate_details')}>
+
                 <View style={{ borderRadius: 60, backgroundColor: COLORS.bg_tile_Colo, flexDirection: 'row', height: 80, alignItems: 'center', elevation: 6, justifyContent: 'space-evenly', marginTop: 15 }}>
                     <View>
                         <Image source={candidateIcon} style={{ width: 50, height: 50, }} />
                     </View>
 
                     <View >
-                        <Text style={{ color: COLORS.black, fontSize: 16, fontWeight: '500' }}>
-                            {name}
-                        </Text>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Icon name="briefcase-variant-outline" color='white' size={30} style={{
-                                marginLeft: 20,
-                                marginTop: 5
-                            }} />
-                            <Text style={{ color: COLORS.gray, fontSize: 12, fontWeight: '500', marginTop: -5 }}>
-                                {designation}
+                        <Text style={{ color: COLORS.black, fontSize: 16, fontWeight: '500', }}>{name}</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                            <Icon name="briefcase-variant-outline" color={COLORS.gray} size={20} />
+                            <Text style={{ color: COLORS.gray, fontSize: 13, fontWeight: '500', marginHorizontal: 1 }}> {designation}
                             </Text>
-
                         </View>
+                        {date ? <Text style={{ color: COLORS.gray, fontSize: 12, }}>       {date}</Text> : null}
                     </View>
+
                     <View style={{ justifyContent: 'flex-end' }}>
                         <Text style={{ backgroundColor: '#8467D7', borderRadius: 20, textAlign: 'center', alignSelf: 'center', padding: 8, color: COLORS.white, fontWeight: '500' }}>
-                            {timing}
+                            {interviewStart} - {interviewEndTime}
                         </Text>
                     </View>
 
@@ -49,69 +64,50 @@ const Interview_status = () => {
     }
 
     return (
-        <View>
-            <View style={{ backgroundColor: COLORS.white, margin: 10, elevation: 6, padding: 8, borderRadius: 10, height: '100%', }}>
+        <ScrollView>
+            <View style={[{ backgroundColor: COLORS.white, flex: 1, margin: 10, padding: 4, borderRadius: 10, height: '100%' }, styles.Elevation]}>
 
 
                 {/* top buttons */}
-                <View style={{ marginTop: 20, marginBottom: 20 }}>
-                    <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', marginHorizontal: 10, }}>
-                        <Text style={[styles.regilizationBtn, { backgroundColor: COLORS.voilet, borderColor: COLORS.white, elevation: 6 }]}
-                            onPress={() => props.navigation.navigate("Apply_Leave")}>
+                <View style={{ marginVertical: 10 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', marginHorizontal: 10, }}>
+                        <TouchableOpacity onPress={() => setStatus("P")}>
+                            <Text style={[styles.Elevation, styles.regilizationBtn, {color:(status==='P'?COLORS.white:COLORS.voilet),borderColor:(status==='P'?COLORS.white:COLORS.voilet),backgroundColor:(status === 'P' ? COLORS.voilet : COLORS.white)}]} >
+
                             Schedule Interview </Text>
-                        <Text style={[styles.regilizationBtn, { backgroundColor: COLORS.white, borderColor: COLORS.voilet, color: COLORS.voilet, elevation: 6 }]}>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setStatus("C")}>
+                        <Text style={[styles.Elevation, styles.regilizationBtn, {color:(status==='C'?COLORS.white:COLORS.voilet),borderColor:(status==='C'?COLORS.white:COLORS.voilet),backgroundColor:(status === 'C' ? COLORS.voilet : COLORS.white)}]}>
                             Complete Interview </Text>
-                    </View>
-
+                    </TouchableOpacity>
                 </View>
-                {/* ListView */}
-
-                <ScrollView>
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap', marginHorizontal: 10 }}>
-                        {InterviewStatusList.map((item) => <CandidateList id={item.id} name={item.name} designation={item.designation} timing={item.timing} date={item.date} image={item.image} />)}
-                    </View>
-
-                </ScrollView>
 
             </View>
+            {/* ListView  */}
+
+            <View style={{ flex: 1, flexDirection: 'row', padding: 8, justifyContent: 'space-between' }}>
+                <ScrollView >
+
+                    {interViewDetail?.filter((item) => item.STATUS === status).map((item) => <CandidateList id={item.INTERVIEW_ID} name={item.CANDIDATE_NAME} designation={item.JOB_TITLE} startTime={item.SCHEDULE_TIME_FROM} endTime={item.SCHEDULE_TIME_TO} date={item.SCHEDULED_DATE} image={item.image} />)}
+                </ScrollView>
+            </View>
+
         </View>
+        </ScrollView >
     )
 }
 
-
-
-
-
 const styles = StyleSheet.create({
-    newJobOpeneingTxt: {
-        padding: 10,
-        fontSize: 14,
-        fontWeight: '500',
-        flexDirection: 'row'
-    },
-    dropDownStyle: {
-        width: '100%',
-        backgroundColor: 'white',
-        borderColor: COLORS.skyBlue,
-        height: 40,
-        elevation: 5,
-        color: COLORS.voilet,
-        marginTop: 5,
-        borderWidth: 1,
-    },
     regilizationBtn: {
-        paddingLeft: 8,
-        paddingRight: 8,
+        paddingHorizontal: 8,
         textAlign: 'center',
-        elevation: 6,
         color: 'white',
         flex: 1,
-        textAlign: 'center',
         marginHorizontal: 5,
         borderRadius: 20,
         borderWidth: 1,
-        paddingTop: 8,
-        paddingBottom: 8,
+        paddingVertical: 8,
         fontSize: 12,
         fontWeight: 500
     },
@@ -123,7 +119,7 @@ const styles = StyleSheet.create({
             height: 2,
         },
         shadowOpacity: 0.3,
-        shadowRadius: 4,
+        shadowRadius: 10,
     },
 })
 
