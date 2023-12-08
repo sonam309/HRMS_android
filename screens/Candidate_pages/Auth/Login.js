380edf52583d3e8ed1def3d86a23d3f60647f733
sonam309 <sona.magics@gmail.com>
2023-12-08 18:27:38 +0530

    added validation on Candidate login and forgot password

--- screens/Candidate_pages/Auth/Login.js
+++ screens/Candidate_pages/Auth/Login.js
@@ -34,7 +34,10 @@ import {
   responsiveWidth,
 } from 'react-native-responsive-dimensions';
 import Toast from 'react-native-toast-message';
-import { COLORS, FONTS, SIZES } from '../../../constants';
+import {COLORS, FONTS, SIZES, icons} from '../../../constants';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import FormInput from '../../../components/FormInput';
 
 const Login = props => {
   let page = null;
@@ -49,18 +52,38 @@ const Login = props => {
   const [openVersionModal, setOpenVersionModal] = useState(true);
   const [newApkVersion, setNewApkVersion] = useState('');
 
+  const userInfo = {
+    userId: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    userId: Yup.string()
+      .trim()
+      .required('CandidateId is required')
+      .matches(/^[a-zA-Z0-9]+$/, 'Invalid candidate id'),
+
+    password: Yup.string().trim().required('Password is required'),
+    // .matches(
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+    //   'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
+    // ),
+  });
+
   const getType = async () => {
     page = await AsyncStorage.getItem('type');
     {
       page ? (page === 'employee' ? setOperFlag('E') : setOperFlag('A')) : null;
     }
   };
+
   const userData = {
     loginId: userId,
     password: password,
     oprFlag: 'L',
     oldPassword: '',
   };
+
   //Random Number
   const RandomNumber = length => {
     return Math.floor(
@@ -84,7 +107,7 @@ const Login = props => {
   };
 
   // forgetpassword
-  const forgetPasswordApi = () => {
+  const forgetPasswordApi = (userId) => {
     setLoaderVisible(true);
     let otp = RandomNumber('6');
     console.log('msg', otp + ' $ ' + userId + operFlag);
@@ -118,83 +141,82 @@ const Login = props => {
   };
 
   // logging in function
-  const submit = () => {
-    if (userId === '' || password === '') {
-      Toast.show({
-        type: 'error',
-        text1: 'UserId and Password is Mandatory !',
-      });
-    } else {
-      console.log('userLogReq', userData);
-      setLoaderVisible(true);
-      axios
-        .post(`${API}/api/User/candidateLogin`, userData)
-        .then(response => {
-          const returnedData = response.data.Result[0];
-          let candidateName = returnedData.CANDIDATE_NAME;
-          let candidateStatus = returnedData.CANDIDATE_STATUS;
-          let candidateRole = returnedData.JOB_TITLE;
-          let candidatePhone = returnedData.PHONE;
-          let candidateRoleId = returnedData.ROLE_ID;
-          let candidateStatusId = returnedData.STATUS_ID;
-          let offerAcceptFlag = returnedData.OFER_ACPT_FLAG;
-          let daysToJoin = returnedData.DAY_TO_JOIN;
-          let candidateOfferLetter = returnedData.OFFER_LETTER;
-          let growingDays = returnedData.GROWING_DAY;
-          let totalDay = returnedData.TOTAL_DAY;
-          let hiringLeadMail = returnedData.HIRING_LEAD_EMAIL;
-          let candidateId = returnedData.USER_ID;
-          let satyaSathiVersion = returnedData.SATHI_VERSION;
+  const submit = values => {
+    const userData = {
+      loginId: values.userId,
+      password: values.password,
+      oprFlag: 'L',
+      oldPassword: '',
+    };
+    console.log('userLogReq', userData);
+    setLoaderVisible(true);
+    axios
+      .post(`${API}/api/User/candidateLogin`, userData)
+      .then(response => {
+        const returnedData = response.data.Result[0];
+        let candidateName = returnedData.CANDIDATE_NAME;
+        let candidateStatus = returnedData.CANDIDATE_STATUS;
+        let candidateRole = returnedData.JOB_TITLE;
+        let candidatePhone = returnedData.PHONE;
+        let candidateRoleId = returnedData.ROLE_ID;
+        let candidateStatusId = returnedData.STATUS_ID;
+        let offerAcceptFlag = returnedData.OFER_ACPT_FLAG;
+        let daysToJoin = returnedData.DAY_TO_JOIN;
+        let candidateOfferLetter = returnedData.OFFER_LETTER;
+        let growingDays = returnedData.GROWING_DAY;
+        let totalDay = returnedData.TOTAL_DAY;
+        let hiringLeadMail = returnedData.HIRING_LEAD_EMAIL;
+        let candidateId = returnedData.USER_ID;
+        let satyaSathiVersion = returnedData.SATHI_VERSION;
 
-          setLoaderVisible(false);
-          setLoginResponse(returnedData);
+        setLoaderVisible(false);
+        setLoginResponse(returnedData);
 
-          console.log('loginresp', returnedData);
+        console.log('loginresp', returnedData);
 
-          if (returnedData.FLAG === 'S') {
-            if (returnedData?.SATHI_VERSION > VERSIONS?.android) {
-              setGreaterVersion(true);
-              console.log(returnedData?.SATHI_VERSION);
-              setNewApkVersion(returnedData?.SATHI_VERSION);
-            } else {
-              dispatch(
-                candidateAuthActions.logIn({
-                  candidateId: returnedData.USER_ID,
-                  candidateName,
-                  candidateRole,
-                  candidateStatus,
-                  candidatePhone,
-                  candidateRoleId,
-                  candidateStatusId,
-                  offerAcceptFlag,
-                  daysToJoin,
-                  candidateOfferLetter,
-                  growingDays,
-                  totalDay,
-                  hiringLeadMail,
-                  satyaSathiVersion,
-                  candidateAuthenticated: true,
-                }),
-              );
-            }
+        if (returnedData.FLAG === 'S') {
+          if (returnedData?.SATHI_VERSION > VERSIONS?.android) {
+            setGreaterVersion(true);
+            console.log(returnedData?.SATHI_VERSION);
+            setNewApkVersion(returnedData?.SATHI_VERSION);
           } else {
-            Toast.show({
-              type: 'error',
-              text1: 'Failure Please enter correct credentials',
-            });
+            dispatch(
+              candidateAuthActions.logIn({
+                candidateId: returnedData.USER_ID,
+                candidateName,
+                candidateRole,
+                candidateStatus,
+                candidatePhone,
+                candidateRoleId,
+                candidateStatusId,
+                offerAcceptFlag,
+                daysToJoin,
+                candidateOfferLetter,
+                growingDays,
+                totalDay,
+                hiringLeadMail,
+                satyaSathiVersion,
+                candidateAuthenticated: true,
+              }),
+            );
           }
-
-          setUserId('');
-          setPassword('');
-        })
-        .catch(error => {
-          setLoaderVisible(false);
+        } else {
           Toast.show({
             type: 'error',
-            text1: error,
+            text1: 'Failure Please enter correct credentials',
           });
+        }
+
+        // setUserId('');
+        // setPassword('');
+      })
+      .catch(error => {
+        setLoaderVisible(false);
+        Toast.show({
+          type: 'error',
+          text1: error,
         });
-    }
+      });
   };
 
   return (
@@ -241,108 +263,197 @@ const Login = props => {
           resizeMode="stretch"
         />
       </View>
-      {/* candidate Login titlte */}
-      <View
-        style={{
-          justifyContent: 'center',
-          flex: 1.5,
-          borderRadius: 20,
-          backgroundColor: COLORS.white,
-          paddingHorizontal: 25,
+      <Formik
+        initialValues={userInfo}
+        validationSchema={validationSchema}
+        onSubmit={(values, formikActions, touched, isValid) => {
+          // onLogin(values, formikActions, touched, isValid);
+          submit(values, formikActions, touched, isValid);
         }}>
-        <Text style={styles.header}>Candidate Login</Text>
+        {({
+          values,
+          errors,
+          handleChange,
+          touched,
+          handleBlur,
+          handleSubmit,
+          isValid,
+          dirty,
+          isSubmitting,
+        }) => {
+          const {userId, password} = values;
+          return (
+            // {/* candidate Login titlte */}
+            <View
+              style={{
+                justifyContent: 'center',
+                flex: 1.5,
+                borderRadius: 20,
+                backgroundColor: COLORS.white,
+                paddingHorizontal: 25,
+              }}>
+              <Text style={styles.header}>Candidate Login</Text>
 
-        {/* user credentials - userId */}
-        <View style={styles.textInputBox}>
-          <CustomInput
-            placeholder={'Candidate Id'}
-            caption={'Candidate ID'}
-            value={userId}
-            onChangeText={name => setUserId(name)}
-            required
-          />
-        </View>
-        {/* Password */}
-        <View style={[styles.textInputBox]}>
-          <CustomInput
-            placeholder={'Password'}
-            caption={'Password'}
-            value={password}
-            onChangeText={security => setPassword(security)}
-            required
-            secureTextEntry={showVisibility}
-            isPasswordInput
-            textInputStyle={{
-              width: responsiveWidth(70),
-            }}
-            icon={
-              <Pressable onPress={changeVisibility}>
-                <AntDesign name="eye" size={22} />
-              </Pressable>
-            }
-          />
-        </View>
+              {/* user credentials - userId */}
+              <View style={styles.textInputBox}>
+                {/* <CustomInput
+                  placeholder={'Candidate Id'}
+                  caption={'Candidate ID'}
+                  value={userId}
+                  onChangeText={name => setUserId(name)}
+                  required
+                /> */}
+                <FormInput
+                  label="Candidate Id"
+                  placeholder={'Candidate Id'}
+                  // keyboardType="email-address"
+                  // autoCompleteType="email"
+                  onChange={handleChange('userId')}
+                  errorMsg={errors.userId}
+                  onBlur={handleBlur('userId')}
+                  labelColor={COLORS.black}
+                  appendComponent={
+                    <View
+                      style={{
+                        justifyContent: 'center',
+                      }}>
+                      <Image
+                        source={
+                          userId == '' ||
+                          (userId != '' && errors.userId == null)
+                            ? icons.correct
+                            : icons.cross
+                        }
+                        style={{
+                          height: 20,
+                          width: 20,
+                          tintColor:
+                            userId == ''
+                              ? COLORS.gray
+                              : userId != '' && errors.userId == null
+                              ? COLORS.green
+                              : COLORS.red,
+                        }}
+                      />
+                    </View>
+                  }
+                  value={values.userId}
+                />
+              </View>
+              {/* Password */}
+              <View style={[styles.textInputBox]}>
+                {/* <CustomInput
+                  placeholder={'Password'}
+                  caption={'Password'}
+                  value={password}
+                  onChangeText={security => setPassword(security)}
+                  required
+                  secureTextEntry={showVisibility}
+                  isPasswordInput
+                  textInputStyle={{
+                    width: responsiveWidth(70),
+                  }}
+                  icon={
+                    <Pressable onPress={changeVisibility}>
+                      <AntDesign name="eye" size={22} />
+                    </Pressable>
+                  }
+                /> */}
+                <FormInput
+                  label="Password"
+                  placeholder={'Password'}
+                  autoCompleteType="password"
+                  secureTextEntry={showVisibility}
+                  onChange={handleChange('password')}
+                  errorMsg={errors.password}
+                  // onBlur={handleBlur('password')}
+                  labelColor={COLORS.black}
+                  value={values.password}
+                  appendComponent={
+                    <TouchableOpacity
+                      style={{
+                        width: 40,
+                        alignItems: 'flex-end',
+                        justifyContent: 'center',
+                      }}
+                      onPress={() => setShowVisibility(!showVisibility)}>
+                      <Image
+                        source={showVisibility ? icons.eye_close : icons.eye}
+                        style={{
+                          height: 20,
+                          width: 20,
+                          tintColor: COLORS.gray,
+                        }}
+                      />
+                    </TouchableOpacity>
+                  }
+                />
+              </View>
 
-        {/* Quick Pin Option */}
-        {/* <View style={styles.loginOption}>
+              {/* Quick Pin Option */}
+              {/* <View style={styles.loginOption}>
                     <TouchableOpacity style={{ alignItems: 'center' }}>
                         <Image source={Pinlock} style={{ width: 30, height: 30, }} />
                         <Text style={{ color: COLORS.darkGray2, ...FONTS.body5 }}>Quick Pin</Text>
                     </TouchableOpacity>
                 </View> */}
 
-        <TextButton
-          color1={COLORS.green}
-          color2={'#9ADD00'}
-          linearGradientStyle={{
-            marginTop: SIZES.base,
-            marginBottom: SIZES.radius,
-            borderRadius: SIZES.base,
-          }}
-          buttonContainerStyle={{width: responsiveWidth(90), height: 50}}
-          label={'Log In'}
-          labelStyle={{color: COLORS.white}}
-          onPress={() => submit()}
-        />
+              <TextButton
+                color1={COLORS.green}
+                color2={'#9ADD00'}
+                linearGradientStyle={{
+                  marginTop: SIZES.base,
+                  marginBottom: SIZES.radius,
+                  borderRadius: SIZES.base,
+                }}
+                buttonContainerStyle={{width: responsiveWidth(90), height: 50}}
+                label={'Log In'}
+                labelStyle={{color: COLORS.white}}
+                onPress={handleSubmit}
+              />
 
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          {/* signIn for Employee */}
-          <TouchableOpacity
-            onPress={async () => {
-              props.navigation.push('EmployeeTab', {
-                screen: 'EmployeeLogin',
-              });
-              await AsyncStorage.setItem('type', 'employee');
-            }}>
-            <Text
-              style={{
-                color: COLORS.hyperlinkBlue,
-                ...FONTS.h5,
-                fontSize: 14,
-                marginBottom: 100,
-                textDecorationLine: 'underline',
-              }}>
-              Sign In as Employee
-            </Text>
-          </TouchableOpacity>
-
-          {/* Forgot Password */}
-          <TouchableOpacity>
-            <Text
-              style={styles.forgotPassword}
-              onPress={() => {
-                userId !== ''
-                  ? forgetPasswordApi()
-                  : Toast.show({
-                      type: 'error',
-                      text1: 'Please enter User Id',
+              <View
+                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                {/* signIn for Employee */}
+                <TouchableOpacity
+                  onPress={async () => {
+                    props.navigation.push('EmployeeTab', {
+                      screen: 'EmployeeLogin',
                     });
-              }}>
-              Forgot Password?{' '}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+                    await AsyncStorage.setItem('type', 'employee');
+                  }}>
+                  <Text
+                    style={{
+                      color: COLORS.hyperlinkBlue,
+                      ...FONTS.h5,
+                      fontSize: 14,
+                      marginBottom: 100,
+                      textDecorationLine: 'underline',
+                    }}>
+                    Sign In as Employee
+                  </Text>
+                </TouchableOpacity>
+
+                {/* Forgot Password */}
+                <TouchableOpacity>
+                  <Text
+                    style={styles.forgotPassword}
+                    onPress={() => {
+                      values.userId !== ''
+                        ? forgetPasswordApi(values.userId)
+                        : Toast.show({
+                            type: 'error',
+                            text1: 'Please enter User Id',
+                          });
+                    }}>
+                    Forgot Password?{' '}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          );
+        }}
+      </Formik>
 
       {/* Bottom element */}
       <View style={{backgroundColor: COLORS.white, height: 30}}>
